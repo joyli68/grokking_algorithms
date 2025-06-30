58a6199d3898bdd221a9b9f2db2cbf3e16307c09
joyli68 <joyousli247@gmail.com>
2025-06-30 13:52:02 -0700

    chapter 2

---
+++ 2/exercises.py
@@ -0,0 +1,18 @@
+'''
+Exercise 2.1
+List because its easier for adding things
+
+Exercise 2.2
+Linked list because it is good for adding and deleting
+
+Exercise 2.3
+Array because its good for random access
+
+Exercise 2.4
+You need to make sure the indexes don't get mixed up if you insert in the middle of the array
+
+Exercise 2.5
+For searching it would be slower than using an array but faster than using a list. For inserting it would be 
+faster than using an array and faster than using a list.
+
+'''
