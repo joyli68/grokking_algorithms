0d3dd0485d20a95dcc928e0d36c17df566bb3fd2
joyli68 <joyousli247@gmail.com>
2025-06-30 14:03:44 -0700

    chapter 3

---
+++ 3/exercises.py
@@ -0,0 +1,10 @@
+'''
+Exercise 3.1
+The greet function has a variable "name" that is assigned the value Maggie, so it greets the user, Maggie. It then calls the 
+greet2 function, which has the same variable name and value. 
+
+Exercise 3.2
+When you write a recursive function that runs forever, the stack will keep growing until you have no more memory to store
+the stack.
+
+'''
