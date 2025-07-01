eacb4287f3f3e8fba5eeaea0e5ba0cf350aa836f
joyli68 <joyousli247@gmail.com>
2025-07-01 14:36:38 -0700

    exercise 4.4

--- 4/exercise4_4.py
+++ 4/exercise4_4.py
@@ -1,19 +1,10 @@
-def binary_search(array, item, start=0, end=None):
-
-    if end is None:
-        end = len(array) - 1
-
+'''   
     # base case
     if start > end:
         return None
-
-    mid = start + (end - start) // 2
-
-    if array[mid] == item:
-        return mid
-
-    # recursive
+      # recursive
     if item < array[mid]:
         return binary_search(array, item, start, mid - 1)
     else:
         return binary_search(array, item, mid + 1, end)
+'''
